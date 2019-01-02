bf7b52d6629b305679e244a9450e570a9f2438f3
goott6-10 <goott6-10@goott6-10-PC>
2019-01-02 10:03:06 +0900

    new

---
+++ jewelry/src/main/java/com/jewelry/vo/orderViewVo.java
@@ -0,0 +1,135 @@
+package com.jewelry.vo;
+
+import java.util.Date;
+
+public class orderViewVo {
+	private int sotreNo;
+	private String productName;
+	private int detailCarat;
+	private String detailColor;
+	private String detailGold;
+	private String detailSize;
+	private String shape;
+	private String orderCk;
+	private int orderCount;
+	private Date orderCkDate;
+	private Date orderDate;
+	private String orderType;
+	private int price;
+	private String userName;
+	private int orderNo;
+	private int productNo;
+	private int detailNo;
+	private int userNo;
+	
+	public int getSotreNo() {
+		return sotreNo;
+	}
+	public void setSotreNo(int sotreNo) {
+		this.sotreNo = sotreNo;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getDetailCarat() {
+		return detailCarat;
+	}
+	public void setDetailCarat(int detailCarat) {
+		this.detailCarat = detailCarat;
+	}
+	public String getDetailColor() {
+		return detailColor;
+	}
+	public void setDetailColor(String detailColor) {
+		this.detailColor = detailColor;
+	}
+	public String getDetailGold() {
+		return detailGold;
+	}
+	public void setDetailGold(String detailGold) {
+		this.detailGold = detailGold;
+	}
+	public String getDetailSize() {
+		return detailSize;
+	}
+	public void setDetailSize(String detailSize) {
+		this.detailSize = detailSize;
+	}
+	public String getShape() {
+		return shape;
+	}
+	public void setShape(String shape) {
+		this.shape = shape;
+	}
+	public String getOrderCk() {
+		return orderCk;
+	}
+	public void setOrderCk(String orderCk) {
+		this.orderCk = orderCk;
+	}
+	public Date getOrderCkDate() {
+		return orderCkDate;
+	}
+	public void setOrderCkDate(Date orderCkDate) {
+		this.orderCkDate = orderCkDate;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	public String getOrderType() {
+		return orderType;
+	}
+	public void setOrderType(String orderType) {
+		this.orderType = orderType;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public int getOrderCount() {
+		return orderCount;
+	}
+	public void setOrderCount(int orderCount) {
+		this.orderCount = orderCount;
+	}
+	public int getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getProductNo() {
+		return productNo;
+	}
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	public int getDetailNo() {
+		return detailNo;
+	}
+	public void setDetailNo(int detailNo) {
+		this.detailNo = detailNo;
+	}
+	
+	
+}
